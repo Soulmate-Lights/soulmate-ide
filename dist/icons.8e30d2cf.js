b23ab138ae69849bbe3b1eea82579dca30b4ce87
Elliott Kember <elliott.kember@gmail.com>
2020-06-01 22:42:59 -0700

    sexy

--- dist/icons.8e30d2cf.js
+++ dist/icons.8e30d2cf.js
@@ -2677,7 +2677,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64098" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49402" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
