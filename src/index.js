de1e300ae697a7d5c95c90d6198eb32f08b85c90
Elliott Kember <elliott.kember@gmail.com>
2020-06-02 09:04:15 -0700

    Monaco fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "@babel/polyfill";
-
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css"; // optional
 import App from "./App";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.main.js";
+
+self.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId, label) {
+    return "./editor.worker.js";
+  },
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
