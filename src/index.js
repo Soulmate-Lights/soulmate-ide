704b13527b14ae5feef3caa231d3173fa624d481
Elliott Kember <elliott.kember@gmail.com>
2020-06-04 20:20:02 -0700

    Dark toggle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import "@babel/polyfill";
-import React from "react";
 import ReactDOM from "react-dom";
-import "semantic-ui-css/semantic.min.css"; // optional
-import App from "./App";
-import * as monaco from "monaco-editor/esm/vs/editor/editor.main.js";
+import React, { Component, useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Custom from "./components/Custom";
+import Simulator from "./simulator";
+import config from "./auth_config.json";
+import { Auth0Provider } from "./react-auth0-spa";
+import history from "./utils/history";
+
+const onRedirectCallback = (appState) => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+const App = () => {
+  return (
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={window.location.href}
+      onRedirectCallback={onRedirectCallback}
+      audience="https://yellow-boat-0900.auth0.com/api/v2/"
+      responseType="token"
+    >
+      <Simulator />
+    </Auth0Provider>
+  );
+};
 
 self.MonacoEnvironment = {
   getWorkerUrl: function (moduleId, label) {
