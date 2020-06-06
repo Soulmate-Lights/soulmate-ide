cfd4e7fc013b0ac4e16bab38edc48bc6fb813f15
Elliott Kember <elliott.kember@gmail.com>
2020-06-05 20:26:53 -0700

    Config 2

--- src/simulator/Editor.js
+++ src/simulator/Editor.js
@@ -12,31 +12,7 @@ import { Link } from "react-router-dom";
 import { Mode, useLightSwitch } from "use-light-switch";
 import Monaco from "react-monaco-editor";
 import request from "request";
-
-// Hook
-function useDebounce(value, delay) {
-  // State and setters for debounced value
-  const [debouncedValue, setDebouncedValue] = useState(value);
-
-  useEffect(
-    () => {
-      // Update debounced value after delay
-      const handler = setTimeout(() => {
-        setDebouncedValue(value);
-      }, delay);
-
-      // Cancel the timeout if value changes (also on delay change or unmount)
-      // This is how we prevent debounced value from updating if value is changed ...
-      // .. within the delay period. Timeout gets cleared and restarted.
-      return () => {
-        clearTimeout(handler);
-      };
-    },
-    [value, delay] // Only re-call effect if value or delay changes
-  );
-
-  return debouncedValue;
-}
+import useDebounce from "./useDebounce";
 
 const App = ({
   code: originalCode,
