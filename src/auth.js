625bf773ac1af36ec2850c9ad36917891bcd0687
Elliott Kember <elliott.kember@gmail.com>
2020-06-20 14:54:14 -0700

    Small things

--- src/auth.js
+++ src/auth.js
@@ -1,13 +1,12 @@
 const ElectronAuth0Login = require("electron-auth0-login").default;
 
 const auth = new ElectronAuth0Login({
-  // Get these from your Auth0 application console
   auth0Audience: "https://yellow-boat-0900.auth0.com/api/v2/",
   auth0ClientId: "OsKmsunrgzhFv2znzUHpd9JsFSsOl46o",
   auth0Domain: "yellow-boat-0900.auth0.com",
   auth0Scopes: "openid profile offline_access",
   applicationName: "soulmate",
-  useRefreshTokens: true, // add useRefreshTokens: true
+  useRefreshTokens: true,
 });
 
 module.exports = auth;
