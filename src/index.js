bc7f276d58a147d5a7786afe2dc77be81db27c68
Elliott Kember <elliott.kember@gmail.com>
2020-06-20 14:49:01 -0700

    Tidyup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import Simulator from "./simulator";
-
-/* eslint-disable-next-line no-unused-vars */
-import * as monaco from "monaco-editor/esm/vs/editor/editor.main.js";
-
 import "regenerator-runtime/runtime";
-
-const App = () => {
-  return <Simulator />;
-};
+import * as monaco from "monaco-editor/esm/vs/editor/editor.main.js";
 
 self.MonacoEnvironment = {
   getWorker: function (_moduleId, _label) {
@@ -20,7 +13,6 @@ self.MonacoEnvironment = {
 };
 
 monaco.editor.createWebWorker({});
-
 module.hot?.accept();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Simulator />, document.getElementById("root"));
