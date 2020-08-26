1346fb262fe38a4fd4c44d5306457278e77d4dd7
Elliott Kember <elliott.kember@gmail.com>
2020-08-25 19:38:42 -0700

    Fix production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,35 +1,5 @@
 const tailwindcss = require("tailwindcss");
-
-const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
-
 const plugins = [];
 plugins.push(tailwindcss("./tailwind.config.js"));
-
 plugins.push(require("autoprefixer"));
-
-// plugins.push(tailwindcss('tailwind.config.js'))
-// This is if you want to include your custom config
-
-if (!IS_DEVELOPMENT) {
-  const purgecss = require("@fullhuman/postcss-purgecss");
-
-  class TailwindExtractor {
-    static extract(content) {
-      return content.match(/[A-z0-9-:\/]+/g) || [];
-    }
-  }
-
-  plugins.push(
-    purgecss({
-      content: ["src/*.html"],
-      extractors: [
-        {
-          extractor: TailwindExtractor,
-          extensions: ["html"],
-        },
-      ],
-    })
-  );
-}
-
 module.exports = { plugins };
