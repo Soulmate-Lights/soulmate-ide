8e7a04a2eed2674cdf850630f7f589968e1385ae
Elliott Kember <elliott.kember@gmail.com>
2020-08-30 20:07:18 -0700

    Remove header back button

--- src/main/components/Header.js
+++ src/main/components/Header.js
@@ -20,24 +20,6 @@ const Header = ({ title, sections, subtitle, actions, className }) => {
         style={{ WebkitAppRegion: "drag" }}
       >
         <div className="md:flex md:items-center md:justify-between">
-          <nav className="relative z-0 inline-flex shadow-sm mr-4">
-            <button
-              disabled={!lastLocation}
-              onClick={() => history.go(-1)}
-              className={`relative inline-flex items-center px-2 py-2 rounded-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500  transition ease-in-out duration-150 ${
-                !lastLocation && "opacity-25"
-              }`}
-              aria-label="Previous"
-            >
-              <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path
-                  fillRule="evenodd"
-                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </nav>
           <div className="flex-1 min-w-0">
             <span className="text-xl leading-7 text-gray-900 dark-mode:text-white sm:leading-9 sm:truncate flex flex-row items-center">
               {compact(sections).map(({ title, to }, i) => (
