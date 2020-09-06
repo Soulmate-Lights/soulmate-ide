4aafdd41919d0e7734e8b644f7132d8cec7494b4
Elliott Kember <elliott.kember@gmail.com>
2020-09-06 09:21:44 -0700

    Save and go back

--- src/main/config.js
+++ src/main/config.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 import ConfigContainer from "~/containers/config";
+import history from "~/utils/history";
 
 import Header from "./components/Header";
 
@@ -241,7 +242,7 @@ const Config = () => {
                 <div className="py-3 mt-4 text-right">
                   <Link
                     className="px-8 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:shadow-outline-blue focus:bg-indigo-500 active:bg-indigo-600 transition duration-150 ease-in-out"
-                    to="/flash"
+                    onClick={history.back}
                   >
                     Save
                   </Link>
