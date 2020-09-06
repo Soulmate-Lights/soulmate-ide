fc062d36446d488f5cbc7cfdf555f9c24ace52d0
Elliott Kember <elliott.kember@gmail.com>
2020-09-06 09:49:45 -0700

    Download links

--- src/main/dashboard.js
+++ src/main/dashboard.js
@@ -78,7 +78,7 @@ const Dashboard = () => {
             </div>
 
             {((!isElectron() && isWindows()) || isMacintosh()) && (
-              <div className="absolute opacity-25 bottom-8 hover:opacity-100 transition-opacity duration-500">
+              <div className="absolute opacity-75 bottom-8 hover:opacity-100 transition-opacity duration-500">
                 <a
                   className="inline-flex items-center px-4 py-2 mx-4 text-base font-medium text-purple-600 bg-white border border-transparent leading-6 rounded-md hover:text-purple-500 focus:outline-none focus:border-purple-300 focus:shadow-outline-gray active:bg-purple-50 active:text-purple-700 transition duration-150 ease-in-out"
                   href={
@@ -87,7 +87,11 @@ const Dashboard = () => {
                       : "https://editor.soulmatelights.com/download/windows"
                   }
                 >
-                  {isMacintosh() ? <AiFillApple /> : <AiFillWindows />}
+                  {isMacintosh() ? (
+                    <AiFillApple className="mr-2" />
+                  ) : (
+                    <AiFillWindows className="mr-2" />
+                  )}
                   Download the desktop app
                 </a>
                 to upload your patterns right to your Soulmate.
