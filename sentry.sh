20e53a38377a46c6313e447036dae810c87f313e
Elliott Kember <elliott.kember@gmail.com>
2020-09-21 11:17:49 -0700

    Sentry environment name

--- sentry.sh
+++ sentry.sh
@@ -10,5 +10,5 @@ npx sentry-cli releases files $VERSION upload-sourcemaps dist --rewrite
 
 # Finalize the release and mark it deployed
 npx sentry-cli releases finalize $VERSION
-npx sentry-cli releases deploys $VERSION new -e prod
+npx sentry-cli releases deploys $VERSION new -e production
 npx sentry-cli releases set-commits --auto $VERSION
