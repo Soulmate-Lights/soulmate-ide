f62732302565fc51904ba5e308ac7ca7da3cbbcd
Elliott Kember <elliott.kember@gmail.com>
2020-12-08 00:14:17 -0800

    This is hard

--- set-up-arm-packages.sh
+++ set-up-arm-packages.sh
@@ -14,3 +14,5 @@
 (cd ../electron-builder/packages/electron-forge-maker-nsis-web && yalc push)
 (cd ../electron-builder/packages/electron-forge-maker-snap && yalc push)
 (cd ../electron-builder/packages/electron-updater && yalc push)
+
+yalc link app-builder-lib builder-util builder-util-runtime dmg-builder electron-builder electron-publish electron-builder-squirrel-windows electron-forge-maker-appimage electron-forge-maker-nsis electron-forge-maker-nsis-web electron-forge-maker-snap electron-updater
