5322bd09c89e56545157a67ed24f27369f7a15ed
Elliott Kember <elliott.kember@gmail.com>
2020-12-21 07:48:43 -0800

    app fix

--- src/main/utils/isDev.js
+++ src/main/utils/isDev.js
@@ -2,6 +2,6 @@ let isDev = false;
 if (window.location.host === "localhost:3000") {
   isDev = true;
 } else if (typeof electron !== "undefined") {
-  isDev = electron?.require("remote").require("electron-is-dev");
+  isDev = electron.remote.require("electron-is-dev");
 }
 export default isDev;
