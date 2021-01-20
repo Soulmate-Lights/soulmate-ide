828ed3dbeb5af635784ae62e6fb7f0baa18ce2cb
Elliott Kember <elliott.kember@gmail.com>
2021-01-20 12:08:11 -0800

    afterPack

--- afterPack.js
+++ afterPack.js
@@ -4,7 +4,6 @@ const { execSync } = child_process;
 exports.default = function (_context) {
   // your custom code
   console.log("Starting afterPack steps =====");
-  // execSync("yarn node-gyp clean && yarn electron-rebuild . && yarn build");
-  execSync("yarn node-gyp clean && yarn build");
+  execSync("yarn node-gyp clean && yarn electron-rebuild . && yarn build");
   console.log("Finished afterPack steps =====");
 };
