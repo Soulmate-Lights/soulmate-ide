a1457ae2fb5d5536afa1441a07b17b4afdc2215a
Elliott Kember <elliott.kember@gmail.com>
2021-01-19 17:46:53 -0800

    Trying to fix build problems

--- afterPack.js
+++ afterPack.js
@@ -4,6 +4,7 @@ const { execSync } = child_process;
 exports.default = function (_context) {
   // your custom code
   console.log("Starting afterPack steps =====");
-  execSync("yarn node-gyp clean && yarn electron-rebuild . && yarn build");
+  // execSync("yarn node-gyp clean && yarn electron-rebuild . && yarn build");
+  execSync("yarn node-gyp clean && yarn build");
   console.log("Finished afterPack steps =====");
 };
