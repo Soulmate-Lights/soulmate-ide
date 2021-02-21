5c8dbfecb4ec0fa7c59f79596414219bca63ee46
Elliott Kember <elliott.kember@gmail.com>
2021-02-21 09:56:45 -0800

    Settings labels

--- src/main/Settings.js
+++ src/main/Settings.js
@@ -1,70 +1,79 @@
 import Header from "./components/Header";
 import NetworkContainer from "./containers/network";
 
-const Options = ({ options, selectedOption, onChange }) => (
-  <>
-    {options.map((option, i) => (
-      <li key={i}>
-        <div
-          className={classnames(
-            "relative flex flex-col p-4 border  md:pl-4 md:pr-6 md:grid md:grid-cols-3",
-            {
-              "bg-indigo-50 border-indigo-200 z-10": selectedOption === option,
-              "border-gray-200": selectedOption === option,
-            }
-          )}
-        >
-          <label className="flex items-center text-sm cursor-pointer">
-            <input
-              aria-describedby="plan-option-pricing-0 plan-option-limit-0"
-              checked={selectedOption === option}
-              className="w-4 h-4 text-indigo-600 border-gray-300 cursor-pointer focus:ring-indigo-500"
-              onChange={() => onChange(option)}
-              type="radio"
-            />
-            <span className="ml-3 font-medium text-gray-900">{option}</span>
-          </label>
-        </div>
-      </li>
-    ))}
+const Options = ({ options, selectedOption, onChange }) => {
+  const optionSelected = options
+    .map(({ option }) => option)
+    .includes(selectedOption);
+  return (
+    <>
+      {options.map(({ option, name }, i) => (
+        <li key={i}>
+          <div
+            className={classnames(
+              "relative flex flex-col p-4 border sm:pl-4 sm:pr-6 sm:grid sm:grid-cols-3",
+              {
+                "bg-indigo-50 border-indigo-200 z-10":
+                  selectedOption === option,
+                "border-gray-200": selectedOption === option,
+              }
+            )}
+          >
+            <label className="flex items-center text-sm cursor-pointer">
+              <input
+                aria-describedby="plan-option-pricing-0 plan-option-limit-0"
+                checked={selectedOption === option}
+                className="w-4 h-4 text-indigo-600 border-gray-300 cursor-pointer focus:ring-indigo-500"
+                onChange={() => onChange(option)}
+                type="radio"
+              />
+              <span className="ml-3 text-sm font-medium text-gray-900">
+                {name}
+              </span>
+            </label>
 
-    <li>
-      <div
-        className={classnames(
-          "relative flex flex-col px-4 border md:pl-4 md:pr-6 md:grid md:grid-cols-3",
-          {
+            <span className="ml-3 text-sm font-medium text-gray-900">
+              {option}
+            </span>
+          </div>
+        </li>
+      ))}
+
+      <li>
+        <div
+          className={classnames("relative flex flex-col px-4 border", {
             "bg-indigo-50 border-indigo-200 z-10": !options.includes(
               selectedOption
             ),
             "border-gray-200": options.includes(selectedOption),
-          }
-        )}
-      >
-        <label
-          className="flex items-center text-sm cursor-pointer"
-          onMouseDown={() => {
-            if (options.includes(selectedOption)) onChange("");
-          }}
+          })}
         >
-          <input
-            aria-describedby="plan-option-pricing-0 plan-option-limit-0"
-            checked={!options.includes(selectedOption)}
-            className="w-4 h-4 mr-4 text-indigo-600 border-gray-300 cursor-pointer focus:ring-indigo-500"
-            disabled
-            type="radio"
-          />
-          <input
-            aria-describedby="plan-option-pricing-0 plan-option-limit-0"
-            className="w-4 w-full h-4 py-4 pl-1 my-2 -ml-2 text-indigo-600 bg-transparent border-gray-300 cursor-pointer focus:ring-indigo-500"
-            onChange={(e) => onChange(e.target.value)}
-            placeholder="Something else"
-            value={!options.includes(selectedOption) ? selectedOption : ""}
-          />
-        </label>
-      </div>
-    </li>
-  </>
-);
+          <label
+            className="flex items-center text-sm cursor-pointer"
+            onMouseDown={() => {
+              if (options.includes(selectedOption)) onChange("");
+            }}
+          >
+            <input
+              aria-describedby="plan-option-pricing-0 plan-option-limit-0"
+              checked={!optionSelected}
+              className="w-4 h-4 mr-4 text-indigo-600 border-gray-300 cursor-pointer focus:ring-indigo-500"
+              disabled
+              type="radio"
+            />
+            <input
+              aria-describedby="plan-option-pricing-0 plan-option-limit-0"
+              className="w-4 w-full h-4 py-4 pl-1 my-2 -ml-2 text-indigo-600 bg-transparent border-gray-300 cursor-pointer focus:ring-indigo-500"
+              onChange={(e) => onChange(e.target.value)}
+              placeholder="Something else"
+              value={!optionSelected ? selectedOption : ""}
+            />
+          </label>
+        </div>
+      </li>
+    </>
+  );
+};
 
 const Settings = () => {
   const {
@@ -87,8 +96,15 @@ const Settings = () => {
             <Options
               onChange={setAppServer}
               options={[
-                "https://editor.soulmatelights.com/",
-                "http://localhost:3001/",
+                {
+                  option: "https://editor.soulmatelights.com/",
+                  name: "Production",
+                },
+                {
+                  option: "https://soulmate-staging.herokuapp.com/",
+                  name: "Staging",
+                },
+                { option: "http://localhost:3001/", name: "Local" },
               ]}
               selectedOption={appServer}
             />
@@ -100,9 +116,15 @@ const Settings = () => {
             <Options
               onChange={setSimulator}
               options={[
-                "https://editor.soulmatelights.com/sketches/build",
-                "https://firmware.soulmatelights.com:8082/build",
-                "http://localhost:8080/build",
+                {
+                  option: "https://editor.soulmatelights.com/sketches/build",
+                  name: "Production (cached)",
+                },
+                {
+                  option: "https://firmware.soulmatelights.com:8082/build",
+                  name: "Server",
+                },
+                { option: "http://localhost:8080/build", name: "Local" },
               ]}
               selectedOption={simulator}
             />
@@ -114,8 +136,11 @@ const Settings = () => {
             <Options
               onChange={setFirmware}
               options={[
-                "https://firmware.soulmatelights.com:8083/build",
-                "http://localhost:8081//build",
+                {
+                  option: "https://firmware.soulmatelights.com:8083/build",
+                  name: "Production",
+                },
+                { option: "http://localhost:8081/build", name: "Local" },
               ]}
               selectedOption={firmware}
             />
