fd48fcddfb61e4c5a726f34c8178891588f0e127
Elliott Kember <elliott.kember@gmail.com>
2021-02-20 18:52:31 -0800

    LocalStorage, settings page

--- src/main/containers/network.js
+++ src/main/containers/network.js
@@ -4,16 +4,30 @@ import { createContainer } from "unstated-next";
 import { auth0Promise } from "~/utils/auth";
 import { headersAndCredentials } from "~/utils/network";
 
+const initialSimulatorUrl =
+  localStorage.simulatorUrl ||
+  "https://editor.soulmatelights.com/sketches/build";
+
+const initialFirmwareUrl =
+  localStorage.firmwareUrl || "https://firmware.soulmatelights.com:8083/build";
+
+const initialAppServerUrl =
+  localStorage.appServerUrl || "https://editor.soulmatelights.com/";
+
 function Config() {
-  const [simulator, setSimulator] = useState(
-    "https://editor.soulmatelights.com/sketches/build"
-  );
-  const [firmware, setFirmware] = useState(
-    "https://firmware.soulmatelights.com:8083/build"
-  );
-  const [appServer, setAppServer] = useState(
-    "https://editor.soulmatelights.com/"
-  );
+  const [simulator, setSimulator] = useState(initialSimulatorUrl);
+  const [firmware, setFirmware] = useState(initialFirmwareUrl);
+  const [appServer, setAppServer] = useState(initialAppServerUrl);
+
+  useEffect(() => {
+    localStorage.simulatorUrl = simulator;
+  }, [simulator]);
+  useEffect(() => {
+    localStorage.firmwareUrl = firmware;
+  }, [firmware]);
+  useEffect(() => {
+    localStorage.appServerUrl = appServer;
+  }, [appServer]);
 
   const url = (path) => normalizeUrl(appServer + "/" + path);
 
