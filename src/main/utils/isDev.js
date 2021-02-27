c4a6a4525816a7ce5c52083d2bda40b94182c0ea
Elliott Kember <elliott.kember@gmail.com>
2021-02-27 12:07:35 -0800

    isDev

--- src/main/utils/isDev.js
+++ src/main/utils/isDev.js
@@ -1,7 +1,10 @@
-let isDev = require("electron-is-dev");
+// let isDev = require("electron-is-dev");
+let isDev = false;
+
 if (window.location.host === "localhost:3000") {
   isDev = true;
 } else if (window.location.host.includes(":300")) {
   isDev = true;
 }
+
 export default isDev;
