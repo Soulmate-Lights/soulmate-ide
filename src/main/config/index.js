97a0a0d5cf7f29f73e56427f05e4b157ec19597e
Elliott Kember <elliott.kember@gmail.com>
2021-02-27 08:58:51 -0800

    Restore warning message

--- src/main/config/index.js
+++ src/main/config/index.js
@@ -56,14 +56,14 @@ const Config = () => {
 
   if (needsPython()) return <InstallPython />;
 
-  // if (!selectedSoulmate) {
-  //   return (
-  //     <div className="flex flex-col items-center p-8 mx-auto my-auto text-center bg-white rounded-lg shadow w-72">
-  //       <RiUsbLine className="w-24 h-24 mb-4 opacity-80" />
-  //       <span>Connect your Soulmate with a USB cable to configure.</span>
-  //     </div>
-  //   );
-  // }
+  if (!selectedSoulmate) {
+    return (
+      <div className="flex flex-col items-center p-8 mx-auto my-auto text-center bg-white rounded-lg shadow w-72">
+        <RiUsbLine className="w-24 h-24 mb-4 opacity-80" />
+        <span>Connect your Soulmate with a USB cable to configure.</span>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col flex-shrink min-h-0 p-8 overflow-auto space-y-10">
