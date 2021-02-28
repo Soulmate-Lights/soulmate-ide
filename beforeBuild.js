532fbb1f8f25757fbd39f5afe62ae059f1dcfb86
Elliott Kember <elliott.kember@gmail.com>
2021-02-27 18:11:12 -0800

    it didn't work

--- beforeBuild.js
+++
@@ -1,7 +0,0 @@
-const child_process = require("child_process");
-const { execSync } = child_process;
-
-exports.default = function (_context) {
-  execSync("rm -rf node_modules/monaco-editor/dev");
-  execSync("rm -rf node_modules/monaco-editor/min-maps");
-};
