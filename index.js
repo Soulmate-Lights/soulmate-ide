b65ed062b8c82b4dbd3692aebdb6b202701fc04b
Elliott Kember <elliott.kember@gmail.com>
2021-04-06 08:12:59 -0700

    end /

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 const EDGE_URL = process.env.EDGE_URL || '/';
 
 var html = fs.readFileSync("./build/_index.html").toString()
-html = html.replaceAll("/src", `${  EDGE_URL}src`);
+html = html.replaceAll("/src", `${  EDGE_URL}/src`);
 
 express()
   .use(express.static(path.join(__dirname, 'build')))
