eeac75446751f5fcc948a18ba309ae396129635d
Elliott Kember <elliott.kember@gmail.com>
2021-04-06 07:25:39 -0700

    Fix index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'build')))
-  .get('/', (req, res) => res.render('./build/index.html'))
+  .get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
