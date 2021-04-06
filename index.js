625aac0bf7dff8dd6c4ef7b0d08af5243eca38a0
Elliott Kember <elliott.kember@gmail.com>
2021-04-06 07:32:16 -0700

    CDN

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-
+const fs = require('fs');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const EDGE_URL = process.env.EDGE_URL || '/';
+
+var html = fs.readFileSync("./build/index.html").toString()
+html = html.replaceAll("/src", `${EDGE_URL}src`);
 
 express()
   .use(express.static(path.join(__dirname, 'build')))
-  .get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')))
+  .get('/(*)', (req, res) => res.send(html))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
