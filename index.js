97599c895cc940db867227db577b0b664fffdb9f
Elliott Kember <elliott.kember@gmail.com>
2021-04-11 10:25:30 -0700

    Prerender

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const fs = require('fs');
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const EDGE_URL = process.env.EDGE_URL || '/';
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const EDGE_URL = process.env.EDGE_URL || "";
 
-var html = fs.readFileSync("./build/_index.html").toString()
-html = html.replaceAll("/src", `${  EDGE_URL}/src`);
+var html = fs.readFileSync("./build/_index.html").toString();
+html = html.replaceAll("/src", `${EDGE_URL}/src`);
 
 express()
-  .use(express.static(path.join(__dirname, 'build')))
-  .get('/(*)', (req, res) => res.send(html))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
+  .use(
+    require("prerender-node").set("prerenderToken", process.env.PRERENDER_TOKEN)
+  )
+  .use(express.static(path.join(__dirname, "build")))
+  .get("/(*)", (req, res) => res.send(html))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
