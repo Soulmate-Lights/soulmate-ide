3e1ce590d0ef8f42e4ab6bc797ff8ff41d64148e
Elliott Kember <elliott.kember@gmail.com>
2021-04-12 09:22:56 -0700

    lint

--- src/main/config/index.js
+++ src/main/config/index.js
@@ -1,5 +1,5 @@
-import { Helmet } from "react-helmet";
 import { HiOutlineLightningBolt } from "@react-icons";
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 import InstallPython, { needsPython } from "~/components/InstallPython";
