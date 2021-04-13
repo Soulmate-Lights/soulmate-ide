2921df750ba6c86c40b82368928b6284c9edce39
Elliott Kember <elliott.kember@gmail.com>
2021-04-13 13:26:51 -0700

    Server stuff for heroku

--- server.js
+++ server.js
@@ -3,20 +3,13 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const EDGE_URL = process.env.EDGE_URL || "";
+const PRERENDER_TOKEN = process.env.PRERENDER_TOKEN;
 
-let html;
-if (fs.existsSync("./build/_index.html")) {
-  html = fs.readFileSync("./build/_index.html").toString();
-} else {
-  html = fs.readFileSync("./build/index.html").toString();
-}
-
+let html = fs.readFileSync("./build/index.html").toString();
 html = html.replaceAll("/src", `${EDGE_URL}/src`);
 
 express()
-  .use(
-    require("prerender-node").set("prerenderToken", process.env.PRERENDER_TOKEN)
-  )
+  .use(require("prerender-node").set("prerenderToken", PRERENDER_TOKEN))
   .use(express.static(path.join(__dirname, "build")))
   .get("/(*)", (req, res) => res.send(html))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
