1940fef8d9df4147f5854a51f46b69472f30d0c6
Elliott Kember <elliott.kember@gmail.com>
2021-04-12 21:54:27 -0700

    postcss.config.js

--- postcss.config.js
+++
@@ -1,5 +0,0 @@
-const tailwindcss = require("tailwindcss");
-const plugins = [];
-plugins.push(tailwindcss("./tailwind.config.js"));
-plugins.push(require("autoprefixer"));
-module.exports = { plugins };
