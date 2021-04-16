97c74762f4ecd143c687ccff30351bb53648ccca
Elliott Kember <elliott.kember@gmail.com>
2021-04-15 22:12:01 -0700

    fixed it

--- src/monaco/index.js
+++ src/monaco/index.js
@@ -1,16 +1,7 @@
 import "./monaco-language";
 
 import * as monaco from "monaco-editor";
-import workerUrl from 'url:../editor.worker';
-
-self.MonacoEnvironment = {
-  getWorkerUrl: function (_moduleId, _label) {
-    return new Worker(new URL(workerUrl));
-  },
-};
-
 window.monaco = monaco;
-
 import { language, monarchLanguage } from "./monaco-cpp-arduino";
 const languageExtensionPoint = { id: "soulmate" };
 monaco.languages.register(languageExtensionPoint);
@@ -18,7 +9,7 @@ monaco.languages.setLanguageConfiguration("soulmate", monarchLanguage);
 monaco.languages.onLanguage("soulmate", () => {
   monaco.languages.setMonarchTokensProvider("soulmate", language);
   monaco.editor.createWebWorker({
-    worker: new Worker(new URL(workerUrl), { module: true })
+    worker: new Worker(new URL('./editor.worker', import.meta.url))
   });
 });
 
